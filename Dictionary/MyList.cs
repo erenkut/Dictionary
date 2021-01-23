cbb472ba9fa5e800901ab401a6ef0292119ae5c8
eren kut <eren.kut.15@gmail.com>
2021-01-23 14:52:17 +0300

    Proje dosyası ekle.

---
+++ Dictionary/MyList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyList<TE>
+    {
+        TE[] items;
+        public MyList()
+        {
+            items = new TE[0];
+        }
+        public void Add(TE item)
+        {
+            TE[] tempArray = items;
+            items = new TE[items.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+
+        public int Length
+        {
+            get { return items.Length; }
+        }
+
+        public TE[] Items
+        {
+            get { return items; }
+        }
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string>();
+            names.Add("Eren");
+
+            Console.WriteLine(names.Length);
+
+            names.Add("Erdem");
+
+            Console.WriteLine(names.Length);
+
+            foreach (var name in names.Items)
+            {
+                Console.WriteLine(name);
+            }
+        } 
+    }
+}
